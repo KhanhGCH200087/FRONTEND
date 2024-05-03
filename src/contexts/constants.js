fd1583243fa18c09c51482de35ba030347c36e35
KhanhGCH200087 <khanhpmgch200087@fpt.edu.vn>
2024-05-03 14:04:01 +0700

    Update constants.js

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,5 +1,5 @@
-export const apiUrl = 'http://localhost:4000'
+export const apiUrl = 'https://backend-hww2.onrender.com'
 
-export const LOCAL_URL = 'http://localhost:3000'
+export const LOCAL_URL = 'https://backend-hww2.onrender.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'test'
